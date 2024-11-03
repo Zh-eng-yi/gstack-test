0d76d9766501cdc7b5a5df8de90f29577a652f03
Zhengyi Zhao <ericzhaozzy@outlook.com>
2024-11-02 20:51:59 -0400

    add more files and try to use the libmprompt

--- include/mprompt.h
+++ include/mprompt.h
@@ -108,4 +108,41 @@ mp_decl_export mp_prompt_t* mp_prompt_top(void);
 mp_decl_export mp_prompt_t* mp_prompt_parent(mp_prompt_t* p);
 
 
+
+#include <errno.h>
+#include "internal/util.h"
+#include "internal/longjmp.h"
+#include "internal/gstack.h"
+
+/*------------------------------------------------------------------------------
+   Internal API for in-place growable gstacks
+------------------------------------------------------------------------------*/
+struct mp_gstack_s {
+  mp_gstack_t*  next;               // used for the cache and delay list
+  uint8_t*      full;               // stack reserved memory (including noaccess gaps)
+  ssize_t       full_size;          // (for now always fixed to be `os_gstack_size`)
+  uint8_t*      stack;              // stack inside the full area (without gaps)
+  ssize_t       stack_size;         // actual available total stack size (includes reserved space) (depends on platform, but usually `os_gstack_size - 2*mp_gstack_gap`)
+  ssize_t       initial_commit;     // initial committed memory (usually `os_page_size`)  
+  ssize_t       committed;          // current committed estimate
+  ssize_t       extra_size;         // size of extra allocated bytes.         
+  uint8_t       extra[1];           // extra allocated (holds the mp_prompt_t structure)
+};
+
+typedef struct mp_gstack_s mp_gstack_t;
+typedef struct mp_gsave_s  mp_gsave_t;
+
+bool         mp_gstack_init(const mp_config_t* config); // normally called automatically
+void         mp_gstack_clear_cache(void);               // clear thread-local cache of gstacks (called automatically on thread termination)
+
+mp_gstack_t* mp_gstack_alloc(ssize_t extra_size, void** extra); 
+void         mp_gstack_free(mp_gstack_t* gstack, bool delay);
+void         mp_gstack_enter(mp_gstack_t* g, mp_jmpbuf_t** return_jmp, mp_stack_start_fun_t* fun, void* arg);
+
+mp_gsave_t*  mp_gstack_save(mp_gstack_t* gstack, uint8_t* sp);    // save up to the given stack pointer (that should be in `gstack`)
+void         mp_gsave_restore(mp_gsave_t* gsave);
+void         mp_gsave_free(mp_gsave_t* gsave);
+
+mp_gstack_t* mp_gstack_current(void);             // implemented in <mprompt.c>
+
 #endif
--- src/test.c
+++ src/test.c
@@ -1,68 +1,31 @@
+// #include "internal/gstack.h"
 #include <stdlib.h>
-#include <stdio.h>
-#include <stdint.h>
+#include <stdbool.h>
 #include <mprompt.h>
-#include <stdatomic.h>
-
-#define N 1000       // max active async workers
-#define M 1000000    // total number of requests
-
-static void* await_result(mp_resume_t* r, void* arg) {
-  (void)(arg);
-  return r;  // instead of resuming ourselves, we return the resumption as a "suspended async computation" (A)
-}
-
-static void* async_worker(mp_prompt_t* parent, void* arg) {
-  (void)(arg);
-  // start a fresh worker
-  // ... do some work
-  intptr_t partial_result = 0;
-  // and await some request; we do this by yielding up to our prompt and running `await_result` (in the parent context!)
-  mp_yield( parent, &await_result, NULL );
-  // when we are resumed at some point, we do some more work 
-  // ... do more work
-  partial_result++;
-  // and return with the result (B)
-  return (void*)(partial_result);
-}
-
-static void async_workers(void) {
-  mp_resume_t** workers = (mp_resume_t**)calloc(N,sizeof(mp_resume_t*));  // allocate array of N resumptions
-  intptr_t count = 0;
-  for( int i = 0; i < M+N; i++) {  // perform M connections
-    int j = i % N;               // pick an active worker
-    // if the worker is actively waiting (suspended), resume it
-    if (workers[j] != NULL) {  
-      count += (intptr_t)mp_resume(workers[j], NULL);  // (B)
-      workers[j] = NULL;
-    }
-    // and start a fresh worker and wait for its first yield (suspension). 
-    // the worker returns its own resumption as a result.
-    if (i < M) {
-      workers[j] = (mp_resume_t*)mp_prompt( &async_worker, NULL );  // (A)
-    }
-  }
-  printf("ran %zd workers\n", count);
-}
-
-#define my_atomic(name) atomic_##name
-#define my_atomic_load(p) my_atomic(load)(p)
-#define my_atomic_load_ptr(tp,p) my_atomic_load(p)
-typedef struct my_type_s {
-  bool a;
-  int b;
-} my_type_t;
-
+#include <stdio.h>
 #include <string.h>
+#include <stdint.h>
 
 int main() {
-  // async_workers();
-  _Atomic(my_type_t *)my_ptr = malloc(sizeof(my_type_t));
-  my_ptr->a = false;
-  my_ptr->b = 520;
-  my_type_t *second_ptr = my_atomic_load_ptr(my_type_t, &my_ptr);
-  printf("read second ptr: %s\n", second_ptr->a ? "true" : "false");
-  printf("read second ptr: %d\n", second_ptr->b);
-  free(my_ptr);
-  return 0;
+    /*
+    printf("allocating gstack...\n");
+    mp_gstack_t* g = mp_gstack_alloc(0, NULL); 
+
+    printf("stack_size: %zd\n", g->stack_size);
+
+    // printf("writing to stack...\n");
+    // uint8_t *stk = g->stack;
+    // memset(stk, 0, 10);
+
+    char *stk = (char *) g->stack;
+
+    printf("at -1: %c\n", stk[-1]);
+
+    printf("check committed size:\n");
+    printf("initial_commit: %zd\n", g->initial_commit);
+    printf("committed: %zd\n", g->committed);
+
+    printf("freeing gstack...\n");
+    mp_gstack_free(g, false);
+    */
 }
